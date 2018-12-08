4a613a48ae2441bfd03920d8c2ff0e28b24c0560
michaelb <michaelb>
2018-12-08 16:11:34 +1100

    Formating changes only

--- webclone.sh
+++ webclone.sh
@@ -5,7 +5,8 @@ MAX_DOWNLOAD_SPEED="250k" # Was set to 50k
 USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0)' # Windows 10 using Firefox 61
 
 if [ "$#" -eq 1 ]; then
-  DOMAIN=$(echo $1 | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/" | awk -F '.' '{print $(NF-1)"."$(NF)}')
+  DOMAIN=$(echo $1 | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/" \
+  | awk -F '.' '{print $(NF-1)"."$(NF)}')
   echo "==> Downloading website from $1 restricted to domain $DOMAIN"
   wget \
     --recursive \
